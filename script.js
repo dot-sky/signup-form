bcb4ffda92d06aca5638215409d064c6801c3cc6
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-02-03 17:41:32 -0400

    add validation styles to input fields

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+const BLANK_SPACE = "\u00A0";
+
 const inputs = document.querySelectorAll(".form-field input");
 inputs.forEach((elem) => {
     elem.addEventListener("focusout", () =>{
         if (!elem.checkValidity()){
-            elem.style.color = "blue";
+            elem.nextElementSibling.textContent = "*Invalid input";
+            elem.classList.add("invalid-input");
         }
         else{
-            elem.style.color = "green";
+            elem.classList.remove("invalid-input");
+            elem.classList.add("valid-input");
+            elem.nextElementSibling.textContent = BLANK_SPACE;
         }
     })
 })
