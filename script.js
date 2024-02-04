f552c1ec55593dfd788e37f75a604d09b5030ba4
dot-sky <108539845+dot-sky@users.noreply.github.com>
2024-02-04 07:23:51 -0400

    change color palette and add custom error messages to input
    Add color palette variables to css file and update every element color rules to use the new variables

--- script.js
+++ script.js
@@ -2,12 +2,31 @@ const BLANK_SPACE = "\u00A0";
 
 const inputs = document.querySelectorAll(".form-field input");
 inputs.forEach((elem) => {
-    elem.addEventListener("focusout", () =>{
-        if (!elem.checkValidity()){
-            elem.nextElementSibling.textContent = "*Invalid input";
+    elem.addEventListener("focusout", () => {
+        if (!elem.checkValidity()) {
+            const input = elem.getAttribute("id");
+            const errorLabel= elem.nextElementSibling;
+            console.log(input);
+            switch (input) {
+                case "name":
+                    errorLabel.textContent = "This field can't be left empty";
+                    break;
+                case "email":
+                    errorLabel.textContent = "Enter a valid email";
+                    break;
+                case "pwd":
+                    errorLabel.textContent = "Passwords do not match";
+                    break;
+                case "pwd-conf":
+                    errorLabel.textContent = BLANK_SPACE;
+                    break;
+                default:
+                    errorLabel.textContent = "Invalid input";
+                    break;
+            }
             elem.classList.add("invalid-input");
         }
-        else{
+        else {
             elem.classList.remove("invalid-input");
             elem.classList.add("valid-input");
             elem.nextElementSibling.textContent = BLANK_SPACE;
